bb30895857c4d8b30fc02c663f5916d27df018e1
kunal karbhari <fullstackdeveloperkunal18@gmail.com>
2026-07-31 23:11:29 +0530

    Create app.py

---
+++ product_service/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/products")
+def products():
+
+    return {
+        "service": "Product Service"
+    }
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5002
+    )
