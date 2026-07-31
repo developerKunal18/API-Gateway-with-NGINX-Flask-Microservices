ff91b6f85e9b6f156fa8f6be593bd22b6c20127a
kunal karbhari <fullstackdeveloperkunal18@gmail.com>
2026-07-31 23:11:05 +0530

    Create app.py

---
+++ user_service/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/users")
+def users():
+
+    return {
+        "service": "User Service"
+    }
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5001
+    )
